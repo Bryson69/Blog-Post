624206c1d6c2ea6d450371bd94895dde56bb4332
Bryson69 <brysonmundia@gmail.com>
2020-07-19 22:28:33 +0300

    Created a function for picture valition and specified image specifications

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField ,SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
@@ -48,17 +49,18 @@ class UpdateAccountForm(FlaskForm):
 
     email = StringField('Email', validators = [DataRequired(), Email()])
 
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()        
-        if user:
-            raise ValidationError('That username is taken. Please choose a different one')
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one')
 
 
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()        
-        if user:
-            raise ValidationError('That email is taken. Please choose a different one')
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one')

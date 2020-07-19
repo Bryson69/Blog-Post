8ef6c8afb9bc0f48af1cb371efcedac983d4aa96
Bryson69 <brysonmundia@gmail.com>
2020-07-19 22:30:01 +0300

    Created a save picture function and updated our UpdateAccount form function

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,9 @@
-from flask import render_template, url_for, flash, redirect
+import os
+import secrets
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -8,7 +11,7 @@ from flask_login import login_user, current_user, logout_user, login_required
 #  Add dictionary which represents a single blog post"
 posts = [
     {
-        'author': 'Corey Schafer',
+        'author': 'Bryson Mundia',
         'title': 'Blog Post 1',
         'content': 'First Post Content',
         'date_posted': 'June 16 , 2020'
@@ -63,7 +66,8 @@ def login():
         user = User.query.filter_by(email = form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unseccessful. Please check your email and password', 'danger')
     return render_template('login.html', title = 'Login', form = form)
@@ -74,8 +78,38 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/account")
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/images', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    form_picture.save(picture_path)
+    
+    return picture_fn
+
+
+@app.route("/account", methods = ['GET', 'POST'])
+@login_required
 def account():
-    return render_template('account.html', title = 'Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
 
-
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename = 'images/' + current_user.image_file)
+    return render_template('account.html', title = 'Account', image_file = image_file, form = form)
